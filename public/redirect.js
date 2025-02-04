510d1d72de00b92d26eb48c9637ca170afed1961
Dabinci1337 <david@3psychos.de>
2025-02-04 18:24:28 +0100

    files

---
+++ public/redirect.js
@@ -0,0 +1,7 @@
+(function () {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        window.location.href = "https://mobile.free3.org";
+    } else {
+        window.location.href = "https://free3.org";
+    }
+})();
