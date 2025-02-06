f655bd53efc2b42a2b03577c2c5a64091e44ef78
Dabinci1337 <david@3psychos.de>
2025-02-06 21:25:37 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-
-import tailwindcss from '@tailwindcss/vite';
-
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ydavinci.github.io/mobile.ydavinci.github.io/',
   base: '/mobile.ydavinci.github.io/',
   output: 'static',
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [tailwind()]
 });
