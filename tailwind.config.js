b2bd4b9bdf695d0ddd3ec57e5aa3076f65059ddc
Dabinci1337 <david@3psychos.de>
2025-02-06 21:14:01 +0100

    fix

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+      './src/**/*.{astro,html,js,jsx,ts,tsx}', // Adjust the paths based on your project structure
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
